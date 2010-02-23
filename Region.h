01b58b998dc6e10516fd1a87524aae466e9f9885
Lebedev Sergey <sacred9@rambler.ru>
2010-02-23 22:25:42 +0300

    decoration element added to Region class

--- Region.h
+++ Region.h
@@ -1,14 +1,20 @@
-#include "IDraw.h"
+#include "BackgroundObject.h"
 
 class Region : public IDraw
 {
 	sf::Sprite drawingBackground;
+	BackgroundObject* objectDecoration;
+	int numberDecoration;
 
 public:
 	Region();
+	//Region(float globalPositionX, float globalPositionY);
 
 	void setPosition(float globalPositionX, float globalPositionY);
+
 	void setBackground(const sf::Image& sourseImage);
+
+	//void setDecorationPosition(int index, float relativePositionX, float relativePositionY);
 
 	virtual void draw(sf::RenderWindow& drawTarget) const;
 };
